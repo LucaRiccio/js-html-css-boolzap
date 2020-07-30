64a256dee2305b5bfa8ca1f95095fb1de9d4e33b
LucaRiccio <lucariccio888@gmail.com>
2020-07-30 20:30:39 +0200

    Aggiunta funzione ora corrente

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     var clone = $(".template .message").clone();
     clone.addClass("send");
     clone.find(".text-message").append(valore);
-    clone.find(".orario-message").append("15:00");
+    clone.find(".orario-message").append(oraReale());
     $(".contenuto-main").append(clone);
     setTimeout(rispostaAutomatica,2000);
   }
@@ -22,8 +22,18 @@ $(document).ready(function(){
     var cloneDue = $(".template .message").clone();
     cloneDue.addClass("return");
     cloneDue.find(".text-message").append("Ok");
-    cloneDue.find(".orario-message").append("15:01");
+    cloneDue.find(".orario-message").append(oraReale());
     $(".contenuto-main").append(cloneDue);
   }
+
+  function oraReale(){
+    var d = new Date();
+    var ore = d.getHours();
+    var minuti = d.getMinutes();
+    var orario = ore + ":" + minuti;
+    return orario;
+  }
+
+
 
 });
