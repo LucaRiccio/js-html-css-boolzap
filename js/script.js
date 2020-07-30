4eb54b6410a40fcc2119d6c221e27429eccd5f54
LucaRiccio <lucariccio888@gmail.com>
2020-07-30 16:41:48 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
 
+    $("#main-message").keydown(function(){
+    if (event.which == 13 || event.keyCode == 13) {
+      invioMessaggio()
+    }
+  })
 
+  //***FUNZIONI***
 
+  function invioMessaggio(){
+    var valore = $("#main-message").val();
+    var clone = $(".template .message").clone();
+    clone.addClass("send");
+    clone.find(".text-message").append(valore);
+    clone.find(".orario-message").append("15:00");
+    $(".contenuto-main").append(clone);
+    setTimeout(rispostaAutomatica,2000);
+  }
 
-
-
-
-
-
-
-
-  
+  function rispostaAutomatica(){
+    var cloneDue = $(".template .message").clone();
+    cloneDue.addClass("return");
+    cloneDue.find(".text-message").append("Ok");
+    cloneDue.find(".orario-message").append("15:01");
+    $(".contenuto-main").append(cloneDue);
+  }
 
 });
